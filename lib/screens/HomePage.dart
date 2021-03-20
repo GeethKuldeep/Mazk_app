b06942568a1f3584f108a76d2ba551cf01a1266c
GeethKuldeep <geethkuldeep@gmail.com>
2021-03-21 02:23:59 +0530

    Details added✨

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,9 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../LandingPage.dart';
+
+class Post {
+  final String title;
+  final String intruction1;
+  final String intruction2;
+  final Color color;
+
+  Post(this.title, this.intruction1,this.intruction2,this.color);
+}
 
 class HomePage extends StatefulWidget {
   static const String id ='Home_Page';
@@ -24,6 +34,13 @@ class _HomePageState extends State<HomePage> {
   static LatLng center1 = const LatLng(16.998600, 82.243592);
   static LatLng center2 = const LatLng(16.973475,82.237144);
   static LatLng center3 = const LatLng(16.960680,82.235901);
+   bool pressed1 = false;
+  bool pressed2 = false;
+  bool pressed3 = false;
+  bool pressed4 = false;
+  bool pressed5 = false;
+  Color final_color;
+  Map details={"Nike":["-Mask","-Asrogya setu",Colors.red],"Big Bazar":["-Mask","-Asrogya setu",Colors.orange],"Lalitha Jewellers":["-Mask","-Asrogya setu",Colors.lightGreenAccent],"Jack and Jones":["-Mask","-Asrogya setu",Colors.red],};
 
 
   String currentlocation="Current Location";
@@ -32,10 +49,10 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
   _getUserLocation();
-  _onAddMarkerButtonPressed(center,currentlocation,name_place);
-  _onAddMarkerButtonPressed(center1,"SRMT mall","Open");
-  _onAddMarkerButtonPressed(center2,"Dominos","Open");
-  _onAddMarkerButtonPressed(center3,"Appllo hospital","Open");
+  _onAddMarkerButtonPressed(center,currentlocation,name_place,pressed1,1);
+  _onAddMarkerButtonPressed(center1,"SRMT mall","Open",pressed2,2);
+  _onAddMarkerButtonPressed(center2,"Dominos","Open",pressed3,3);
+  _onAddMarkerButtonPressed(center3,"Appllo hospital","Open",pressed4,4);
 
 
 
@@ -78,7 +95,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  _onAddMarkerButtonPressed(LatLng newposition,String title,String snippet){
+  _onAddMarkerButtonPressed(LatLng newposition,String title,String snippet,bool pressed,int a){
     setState(() {
       _markers.add(
           Marker(
@@ -87,6 +104,13 @@ class _HomePageState extends State<HomePage> {
             infoWindow: InfoWindow(
               title: title,
               snippet: snippet,
+              onTap:(){
+                setState(() {
+                  pressed =true;
+                  print('Pressed${a} = ${pressed}');
+                  ontap(context);
+                });
+              }
             ),
             icon: BitmapDescriptor.defaultMarker,
           )
@@ -96,10 +120,125 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void ontap(context){
+    showModalBottomSheet(context: context, builder:(BuildContext bc){
+      return SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height*.60,
+          child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left:26,top:26,right: 26,bottom: 10),
+                      child: Align(
+                          alignment:Alignment.topLeft,
+                          child: Text("SRMT Mall",style:TextStyle(fontSize: 35,fontWeight: FontWeight.bold))),
+                    ),
+                    Padding( padding: const EdgeInsets.only(left:26,top:26,right: 26,bottom: 10),child: Text("L",style:TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize:30 )))
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left:26),
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('-Mask')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left:26),
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('-Aarogya setu')),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left:26),
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('-Covid test certificate')),
+                ),
+                Expanded(
+                  child: SearchBar<Post>(
+                    onSearch: search,
+                    onItemFound: (Post post, int index) {
+                      if(post.color==1){
+                        setState(() {
+                          final_color =Colors.red;
+                        });
+                      }
+                      if(post.color==2){
+                        setState(() {
+                          final_color =Colors.orangeAccent;
+                        });
+                      } if(post.color==3){
+                        setState(() {
+                          final_color =Colors.lightGreenAccent;
+                        });
+                      }
+
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(post.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+                                    Text(post.intruction1,style: TextStyle(fontSize: 15),),
+                                    Text(post.intruction2,style: TextStyle(fontSize: 15),),
+
+
+
+                                  ],
+                                ),
+                                Container(
+
+                                  height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                          color: post.color,
+                                        borderRadius: BorderRadius.all(Radius.circular(20))
+                                    ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+              ],
+            ),
+
+        ),
+      );
+    });
+  }
+
+  Future<List<Post>> search(String search1) async {
+    await Future.delayed(Duration(seconds: 2));
+    return List.generate(details.length, (int index) {
+      return Post(
+        "${details.keys.toList()[index]}",
+        "${details.values.toList()[index][0]}",
+        "${details.values.toList()[index][1]}",
+        details.values.toList()[index][2],
+      );
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
         actions: [
           IconButton(
               icon: Icon(Icons.logout,size:35),
@@ -126,7 +265,7 @@ class _HomePageState extends State<HomePage> {
             Padding(
               padding:EdgeInsets.all(16.0) ,
               child: Align(
-                alignment: Alignment.topRight ,
+                alignment: Alignment.topLeft ,
                 child: Column(
                   children: [
                     button(_onMapTypeButtonPressed, Icons.map),
@@ -136,7 +275,8 @@ class _HomePageState extends State<HomePage> {
                 ),
 
               ),
-            )
+            ),
+
           ],
 
         )
