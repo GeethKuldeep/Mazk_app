8e5caa43678d697ef1735753eb58cf561d477e7f
GeethKuldeep <geethkuldeep@gmail.com>
2021-03-30 22:38:43 +0530

    Schedule page updated✨

--- lib/screens/Schedule.dart
+++ lib/screens/Schedule.dart
@@ -93,9 +93,7 @@ class _ScheduleState extends State<Schedule> {
                   if(snapshot.data.docs[1]["Added"]== "True"){
                     check.add(snapshot.data.docs[1]["StoreName"]);
                   }
-                  if(snapshot.data.docs[2]["Added"]== "True"){
-                    check.add(snapshot.data.docs[2]["StoreName"]);
-                  }
+
                   print("No:of Shops = ${snapshot.data.docs.length}");
                   if (snapshot.data.docs.isEmpty || snapshot.hasError || snapshot.hasData==false) {
                     print("HELLLLO");
@@ -113,98 +111,83 @@ class _ScheduleState extends State<Schedule> {
                     );
                   }
                   else if (snapshot.hasData && snapshot.data != null) {
+                    print(snapshot.data.docs.length);
 
-                    if(check.length!=0){
-                       return ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: snapshot.data.docs.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          DocumentSnapshot user = snapshot.data.docs[index];
-                          if (user["Current_strength"] <=
-                              user["Total_strength"] * 0.25) {
-                            final_color = Colors.lightGreenAccent;
-                          }
-                          else if (user["Total_strength"] * 0.25 <
-                              user["Current_strength"] &&
-                              user["Current_strength"] <=
-                                  user["Total_strength"] * 0.75) {
-                            final_color = Colors.orange;
-                          }
-                          else if (user["Total_strength"] * 0.75 <
-                              user["Current_strength"] &&
-                              user["Current_strength"] <=
-                                  user["Total_strength"]) {
-                            final_color = color1;
-                          }
-                          if(user["StoreName"]=="KFC")
-                            URL1 ="images/kfc.png";
-                          else if(user["StoreName"]=="Nike")
-                            URL1 ="images/nike1.png";
-                          if(user["Added"]=="True"){
-                            print("THERE");
-                          }
-
-
-
-                          if(user["Added"]=="True"){
-                            return  Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.height*0.25,
-                                      child: Row(
-                                        children: [
-                                          Text("${user["Safest_time"]}"),
-                                          SizedBox(width: 15,),
-                                          Text(user["StoreName"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
-                                        ],
-                                      ),
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        DocumentSnapshot user = snapshot.data.docs[index];
+                        if (user["Current_strength"] <=
+                            user["Total_strength"] * 0.25) {
+                          final_color = Colors.lightGreenAccent;
+                        }
+                        else if (user["Total_strength"] * 0.25 <
+                            user["Current_strength"] &&
+                            user["Current_strength"] <=
+                                user["Total_strength"] * 0.75) {
+                          final_color = Colors.orange;
+                        }
+                        else if (user["Total_strength"] * 0.75 <
+                            user["Current_strength"] &&
+                            user["Current_strength"] <=
+                                user["Total_strength"]) {
+                          final_color = color1;
+                        }
+                        if(user["StoreName"]=="KFC")
+                          URL1 ="images/kfc.png";
+                        else if(user["StoreName"]=="Nike")
+                          URL1 ="images/nike1.png";
+                        else if(user["StoreName"]=="Jockey")
+                          URL1 ="images/jockey1.png";
+                        if(user["Added"]=="True"){
+                          print("THERE");
+                        }
+                        if(user["Added"]=="True"){
+                          return  Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.height*0.25,
+                                    child: Row(
+                                      children: [
+                                        Text("${user["Safest_time"]}"),
+                                        SizedBox(width: 15,),
+                                        Text(user["StoreName"], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+                                      ],
                                     ),
-                                    Container(
-                                      height: 25,
-                                      width: 25,
-                                      decoration: BoxDecoration(
-                                          color: Colors.green,
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(20))
-                                      ),
+                                  ),
+                                  Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(20))
                                     ),
+                                  ),
 
 
-                                  ],
-                                ),
-                                SizedBox(height: 15,)
-                              ],
-                            );
-                          }
-                          else if(user["Added"]=="false" ){
-                            return SizedBox(height:0,width:0);
-                          }
-                          else{
-                            return Text("Oops, dont have a schedule yet? Lets build you a custom itenary tailored especially for you. To get started search for the places you would like to visit today.",style: TextStyle(color: Colors.black),);
-                          }
+                                ],
+                              ),
+                              SizedBox(height: 15,)
+                            ],
+                          );
+                        }
+                        else if(user["Added"]=="false" ){
+                          return SizedBox(height:0,width:0);
+                        }
+                        else{
+                          return Text("Oops, dont have a schedule yet? Lets build you a custom itenary tailored especially for you. To get started search for the places you would like to visit today.",style: TextStyle(color: Colors.black),);
+                        }
+
+                      },
 
 
+                    );
 
-
-                        },
-
-
-                      );
-                    }
-                    else{
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            SizedBox(height: MediaQuery.of(context).size.height*0.05,),
-                            Text("Oops, dont have a schedule yet? Lets build you a custom itenary tailored especially for you. To get started search for the places you would like to visit today.",style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),),
-                          ],
-                        ),
-                      );
-                    }
 
                   }
                   else {
@@ -251,10 +234,10 @@ class _ScheduleState extends State<Schedule> {
                       style: ElevatedButton.styleFrom(
                         primary: color2,
                         shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(12.0),
-                      ),),
+                          borderRadius: new BorderRadius.circular(12.0),
+                        ),),
                       onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
 
                       },
                       child: Text("Search for a shop near you",style:TextStyle(color: Colors.white,fontSize: 20))),
